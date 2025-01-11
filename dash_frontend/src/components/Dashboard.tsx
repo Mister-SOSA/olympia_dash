6027941abda601c9b5801f0d03f3cb18f913910a
mister-sosa <karabetsaki@gmail.com>
2025-01-11 05:35:43 -0600

    update widget dimensions in master widget list for improved layout

--- dash_frontend/src/components/Dashboard.tsx
+++ dash_frontend/src/components/Dashboard.tsx
@@ -13,12 +13,12 @@ const LOCAL_STORAGE_KEY = "dashboard_layout";
 // Master list of all possible widgets
 const masterWidgetList: Widget[] = [
     { id: "Overview", x: 0, y: 0, w: 11, h: 2, enabled: true },
-    { id: "SalesByDayBar", x: 0, y: 2, w: 3, h: 3, enabled: true },
-    { id: "SalesByMonthBar", x: 0, y: 0, w: 3, h: 3, enabled: true },
-    { id: "SalesByMonthComparisonBar", x: 6, y: 8, w: 3, h: 3, enabled: false },
-    { id: "ClockWidget", x: 0, y: 0, w: 6, h: 6, enabled: true },
-    { id: "DateWidget", x: 0, y: 0, w: 6, h: 6, enabled: true },
-    { id: "TopCustomersThisYearPie", x: 0, y: 0, w: 12, h: 6, enabled: true },
+    { id: "SalesByDayBar", x: 0, y: 0, w: 5, h: 4, enabled: true },
+    { id: "SalesByMonthBar", x: 0, y: 0, w: 5, h: 4, enabled: true },
+    { id: "SalesByMonthComparisonBar", x: 0, y: 0, w: 5, h: 4, enabled: false },
+    { id: "ClockWidget", x: 0, y: 0, w: 4, h: 2, enabled: true },
+    { id: "DateWidget", x: 0, y: 0, w: 6, h: 2, enabled: true },
+    { id: "TopCustomersThisYearPie", x: 0, y: 0, w: 3, h: 7, enabled: true },
 ];
 
 // Function to read layout from localStorage
