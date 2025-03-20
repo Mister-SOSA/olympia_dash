9ccbf877d3edbe6ce3637ff95e1d5ca149cd2687
mister-sosa <karabetsaki@gmail.com>
2025-03-20 12:46:23 -0500

    fix: adjust font size in TopProductUnitSalesTable for improved readability

--- dash_frontend/src/components/widgets/TopProductUnitSales.tsx
+++ dash_frontend/src/components/widgets/TopProductUnitSales.tsx
@@ -126,7 +126,7 @@ export default function TopProductUnitSalesTable() {
 
         return (
             <ScrollArea className="h-[99%] rounded-md border mt-2">
-                <Table className="text-left text-white outstanding-orders-table text-[.95rem]" wrapperClassName="overflow-clip">
+                <Table className="text-left text-white outstanding-orders-table text-[.9rem]" wrapperClassName="overflow-clip">
                     <TableHeader>
                         <TableRow>
                             <TableHead>Part Code</TableHead>
